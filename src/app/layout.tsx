2e1186ba4bcc5c00dee666315c05d6f81c18c076
Diego-Amieva <diego.amieva.olvera@gmail.com>
2026-06-29 16:08:23 -0600

    feat: replace footer copyright with social icons and inject tracking pixels blueprints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { ContactModalProvider } from '@/providers/ContactModalContext';
 import { GlobalContactModal } from '@/providers/GlobalContactModal';
 import { CookieBanner } from '@/components/ui/CookieBanner';
 import { App } from 'antd';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -32,6 +33,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const fbPixelId = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
+  const linkedInPartnerId = process.env.NEXT_PUBLIC_LINKEDIN_PARTNER_ID;
+
   return (
     <html lang="es" className={inter.variable}>
       <body>
@@ -44,6 +48,71 @@ export default function RootLayout({
             </App>
           </ContactModalProvider>
         </LangProvider>
+
+        {fbPixelId && (
+          <>
+            <Script
+              id="fb-pixel"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '${fbPixelId}');
+                  fbq('track', 'PageView');
+                `,
+              }}
+            />
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                src={`https://www.facebook.com/tr?id=${fbPixelId}&ev=PageView&noscript=1`}
+                alt=""
+              />
+            </noscript>
+          </>
+        )}
+
+        {linkedInPartnerId && (
+          <>
+            <Script
+              id="linkedin-insight"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  _linkedin_partner_id = "${linkedInPartnerId}";
+                  window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                  window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+                  (function(l) {
+                  if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+                  window.lintrk.q=[]}
+                  var s = document.getElementsByTagName("script")[0];
+                  var b = document.createElement("script");
+                  b.type = "text/javascript";b.async = true;
+                  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                  s.parentNode.insertBefore(b, s);})(window.lintrk);
+                `,
+              }}
+            />
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                alt=""
+                src={`https://px.ads.linkedin.com/collect/?pid=${linkedInPartnerId}&fmt=gif`}
+              />
+            </noscript>
+          </>
+        )}
       </body>
     </html>
   );
